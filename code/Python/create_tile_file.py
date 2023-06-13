be5233cf47cee747be700a06a22ff05025bd0a91
paul92219 <119276586+paul92219@users.noreply.github.com>
2023-06-13 09:42:41 -0700

    Countinue working on tilefile app

--- code/Python/create_tile_file.py
+++ code/Python/create_tile_file.py
@@ -1,4 +1,91 @@
+from tkinter import messagebox
+from warnings import resetwarnings
 import customtkinter as ctk
+import json
+
+# Initialize Global Veriables
+tiles_array = 0
+
+"""
+    tile = 
+    {
+        "index": 0,
+        "color": (0, 0, 0),
+        "z_coord": 0
+    }
+
+"""
+
+def get_tiles():
+    global tiles_array
+    with open("tiles_file.json") as file:
+        tiles_array = json.load(file)
+
+def push_tile_to_file():
+    pass
+
+def check_value(value, type):
+    if type == "index":
+        if value >= 65536:
+            messagebox.showwarning("Warning", "Tile Index Can't be bigger then 65536!")
+            return False
+        else:
+            pass
+    elif type == "color":
+        if value >= 256:
+            messagebox.showwarning("Warning", "Tile ColorRange is from 0 to 255!")
+            return False
+        else:
+            pass
+    else:
+        if value > 10:
+            messagebox.showwarning("Warning", "Z coord can't be more then 10!")
+            return False
+        else:
+            pass
+
+def convert_str_to_int(value):
+    result = 0
+    
+    if value != '':
+        try:
+            result = int(value)
+            return result
+
+        except ValueError:
+            messagebox.showwarning("Warning", "The nambers have to be integers")
+    else:
+        pass
+
+def add_tile():
+    global tiles_array
+
+    tile_index = convert_str_to_int(tile_index_entry.get())
+    tile_color_R = convert_str_to_int(tile_red_color_entry.get())
+    tile_color_G = convert_str_to_int(tile_green_color_entry.get())
+    tile_color_B = convert_str_to_int(tile_blue_color_entry.get())
+    tile_z_coord = convert_str_to_int(tile_z_coord_entry.get())
+
+    check_value(tile_index)
+    check_value(tile_color_R)
+    check_value(tile_color_G)
+    check_value(tile_color_B)
+    check_value(tile_z_coord)
+
+    tile = {"index": tile_index, 
+            "color": (tile_color_R, tile_color_G, tile_color_B),
+            "z_coord": tile_z_coord}
+
+def remove_tile():
+    global tiles_array
+    
+    pass
+
+def write_array_for_cpp():
+    pass
+
+# Take Tiles Info
+get_tiles()
 
 # Create the main window
 root = ctk.CTk()
@@ -19,7 +106,7 @@ tile_blue_color_entry = ctk.CTkEntry(entry_frame, width=100, height=30)
 tile_z_coord_entry = ctk.CTkEntry(entry_frame, width=300, height=30)
 
 # Create buttons
-add_button = ctk.CTkButton(entry_frame, text="Add Tile", width=300, height=64, font=("Arial", 20))
+add_button = ctk.CTkButton(entry_frame, text="Add Tile", command=add_tile(), width=300, height=64, font=("Arial", 20))
 remove_button = ctk.CTkButton(entry_frame, text="Remove Tile", width=300, height=64, font=("Arial", 20))
 
 # Create Labels
