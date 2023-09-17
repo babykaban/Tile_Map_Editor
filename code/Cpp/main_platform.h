2dc16c4b852979ec89f04240d4f46f299282dd78
paul92219 <119276586+paul92219@users.noreply.github.com>
2023-09-16 18:04:00 -0700

    Add Memory Alocation And Function That loads tiles

--- code/Cpp/main_platform.h
+++ code/Cpp/main_platform.h
@@ -21,6 +21,7 @@ typedef uint64_t uint64;
     
 typedef float real32;
 typedef double real64;
+typedef size_t memory_index;
 
 #include <stdio.h>
 #include <stdlib.h>
@@ -29,6 +30,11 @@ typedef double real64;
 #define ArrayCount(Array) (sizeof(Array) / sizeof((Array)[0]))
 
 #define Assert(Expression) if(!(Expression)) {*(int *)0 = 0;}
+    
+#define Kilobytes(Value) ((Value)*1024LL)
+#define Megabytes(Value) (Kilobytes(Value)*1024LL)
+#define Gigabytes(Value) (Megabytes(Value)*1024LL)
+#define Terabytes(Value) (Gigabytes(Value)*1024LL)
 
 struct bit_scan_result
 {
@@ -56,6 +62,50 @@ SafeTruncateUInt64(uint64 Value)
     Assert(Value <= 0xFFFFFFFF);
     uint32 Result = (uint32)Value;
     return(Result);
+}
+
+struct memory_arena
+{
+    memory_index Size;
+    uint8 *Base;
+    memory_index Used;
+
+    int32 TempCount;
+};
+
+#define PushStruct(Arena, type) (type *)PushSize_(Arena, sizeof(type))
+#define PushArray(Arena, Count, type) (type *)PushSize_(Arena, (Count)*sizeof(type))
+#define PushSize(Arena, Size) PushSize_(Arena, Size)
+
+inline void
+InitializeArena(memory_arena *Arena, memory_index Size, void *Base)
+{
+    Arena->Size = Size;
+    Arena->Base = (uint8 *)Base;
+    Arena->Used = 0;
+    Arena->TempCount = 0;
+}
+
+inline void *
+PushSize_(memory_arena *Arena, memory_index Size)
+{
+    Assert((Arena->Used + Size) <= Arena->Size);
+    void *Result = Arena->Base + Arena->Used;
+    Arena->Used += Size;
+    
+    return(Result);
+}
+
+#define ZeroStruct(Instance) ZeroSize(sizeof(Instance), &(Instance))
+inline void
+ZeroSize(memory_index Size, void *Ptr)
+{
+    // TODO(casey): Check this guy for performance
+    uint8 *Byte = (uint8 *)Ptr;
+    while(Size--)
+    {
+        *Byte++ = 0;
+    }
 }
 
 #define MAIN_PLATFORM_H
